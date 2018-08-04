443fc14b281fac01f63dbdca40951b03a695b593
Goran <goranmrd@gmail.com>
2018-08-04 12:12:39 +0200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -14,13 +14,6 @@ def post_list(request):
 
 
 @login_required
-def mark_as_read(request):
-    qs = User.objects.get(pk=request.user.pk)
-    qs.notifications.mark_all_as_read()
-    return redirect('/')
-
-
-@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
