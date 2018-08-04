f03ea5837c596bf7d21c1a8145c3ade5950629fa
Goran <goranmrd@gmail.com>
2018-08-04 12:03:39 +0200

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -12,7 +12,5 @@ urlpatterns = [
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
-    path('my_unreads/', views.my_unread_notifications, name='my_unread_notifications'),
     path('signup/', views.signup, name='signup'),
-    path('mark_as_read/', views.mark_as_read, name='mark_as_read'),
 ]
