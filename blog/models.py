95b7e798c7cda5a704d2d0b8be2d59df3e6a8633
Goran <goranmrd@gmail.com>
2018-08-04 09:35:00 +0200

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,7 @@
-from django.db import models
 from django.db import models 
 from django.utils import timezone
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from notifications.signals import notify
+
 
 # Create your models here. 
 class Post(models.Model): 
@@ -37,9 +35,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-
-def my_handler(sender, instance, created, **kwargs):
-    notify.send(instance, recipient=instance.post.author, verb='New comment has been added')
-
-post_save.connect(my_handler, sender=Comment)
