79fd49cf11aeda1b2ddcaba45e387d67157bd5aa
Goran <goranmrd@gmail.com>
2018-08-04 12:00:33 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,9 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize',
     'blog',
-    'notifications',
     'crispy_forms',
 ]
 
